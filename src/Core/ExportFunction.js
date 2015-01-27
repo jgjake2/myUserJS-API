0d47a0429ee4ac57f08b23e3c07a9869ffd5ce40
root <jgjake22@gmail.com>
2015-01-27 17:16:19 -0600

    minor improvements

--- src/Core/ExportFunction.js
+++ src/Core/ExportFunction.js
@@ -2,20 +2,16 @@
 // +@history (0.0.17) Better support when "exportFunction" is unavailable.
 
 function mExportFunction(func, scope, args){
-	if(typeof exportFunction !== _undefined){
+	if(EXISTS(exportFunction)){
 		try{
 			return exportFunction(func, scope, args);
 		}catch(e){}
 	}
 	var name = '';
-	if(typeof args === _undefined) args = {};
-	if(typeof args.defineAs !== _undefined)
+	if(args && args.defineAs)
 		name = args.defineAs;
 	else if(typeof func === "function" && func.name != '')
 		name = func.name
-	if(name == '') return;
-	try{
-		scope[name] = func;
-		return scope[name];
-	}catch(e){}
+	if(name != '')
+		try{return (scope[name] = func);}catch(e){}
 }
